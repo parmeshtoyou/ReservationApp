a1202dbbc2473430fb7cf7f022d80ff499c13537
Parmesh <parmesh.mahore@ril.com>
2023-07-03 22:36:29 +0530

    Initial commit

---
+++ ReservationApp/ReservationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationAppApp.swift
+//  ReservationApp
+//
+//  Created by Parmesh Mahore on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ReservationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
